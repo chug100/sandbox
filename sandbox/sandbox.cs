306e99ae2b1a1b8d0de861599354e43791e75d44
Steven Smith <sdsmith@Stevens-MacBook-Pro-2.local>
2015-12-20 11:02:26 -0500

    First change

--- sandbox/sandbox.cs
+++ sandbox/sandbox.cs
@@ -15,7 +15,7 @@ namespace sandbox
 					Children = {
 						new Label {
 							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
+							Text = "Welcome to The Sandbox!"
 						}
 					}
 				}
